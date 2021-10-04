8312e4ebacc1e27a280abd62685d51ebe9005b84
l3onardodev.github.io <leonardo.contato03@gmail.com>
2021-10-04 15:00:57 -0300

    BREAKING CHANGE: Changes in file's structure. Vue routers added, but not configured yet.

---
+++ src/components/shoes.vue
@@ -0,0 +1,154 @@
+<template>
+    <div class="items">
+        <div class="product-item" v-for="item in items" :key="item.id" @mouseover="item['hover'] = true" @mouseleave="item['hover'] = false">
+            <img v-bind:src="item.image" :title="item.model" class="product-item__image">
+            <div class="product-item-hovered" v-bind:class="{ opacity1: item['hover'] }">
+                <h2 class="product-item-hovered__title">{{ item.model }}</h2>
+                <button class="product-item-hovered__button">ver produto</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                items: [{
+                    id: 1,
+                    model: 'nike air',
+                    price: 300,
+                    description: 'tranquilo',
+                    image: require('../assets/items/1.png'),
+                    hover: false
+                },
+                {
+                    id: 2,
+                    model: 'nike naosei',
+                    price: 450,
+                    description: 'tenis mto bala',
+                    image: require('../assets/items/2.png'),
+                    hover: false
+                },
+                {
+                    id: 3,
+                    model: 'nike pika',
+                    price: 500,
+                    description: 'tenis mto bala',
+                    image: require('../assets/items/3.png'),
+                    hover: false
+                },
+                {
+                    id: 4,
+                    model: 'nike naosei',
+                    price: 450,
+                    description: 'tenis mto bala',
+                    image: require('../assets/items/4.png'),
+                    hover: false
+                },
+                {
+                    id: 5,
+                    model: 'nike naosei',
+                    price: 450,
+                    description: 'tenis mto bala',
+                    image: require('../assets/items/5.png'),
+                    hover: false
+                },
+                {
+                    id: 6,
+                    model: 'nike naosei',
+                    price: 450,
+                    description: 'tenis mto bala',
+                    image: require('../assets/items/6.png'),
+                    hover: false
+                },
+                {
+                    id: 7,
+                    model: 'nike naosei',
+                    price: 450,
+                    description: 'tenis mto bala',
+                    image: require('../assets/items/7.png'),
+                    hover: false
+                },
+                {
+                    id: 8,
+                    model: 'nike naosei',
+                    price: 450,
+                    description: 'tenis mto bala',
+                    image: require('../assets/items/8.png'),
+                    hover: false
+                }]
+            }
+        }
+    }
+</script>
+
+<style>
+    .product-item {
+        height: 302px;
+        width: 24.5%;
+        max-width: 300px;
+        background-color: #EBE9EA;
+        display: flex;
+        border-radius: 2.5px;
+        margin: 1em 0;
+    }
+
+    .product-item__image {
+        width: 100%;
+        object-fit: cover;
+        object-position: center;
+        align-self: center;
+    }
+
+    .items {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap:wrap;
+    }
+
+    .product-item-hovered {
+        position: absolute;
+        height: 302px;
+        width: 21%;
+        max-width: 295px;
+        background-color: rgb(0, 0, 0, 0.65);
+        color: #f6f6f6;
+        opacity: 0;
+        transition: all .25s ease;
+    }
+
+    .opacity1 {
+        opacity: 1;
+    }
+    .product-item-hovered {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .product-item-hovered__title {
+        font-size: 1.5rem;
+    }
+
+    .product-item-hovered__button {
+        border: 2px solid #f6f6f6;
+        background: none;
+        color: #f6f6f6;
+        cursor: pointer;
+        text-transform: uppercase;
+        font-size: 0.9rem;
+        font-family: inherit;
+        font-weight: 600;
+        padding: .5em 1em;
+        margin: 1em 0;
+        transition: all .25s ease;
+    }
+
+    .product-item-hovered__button:hover {
+        background-color: #f6f6f6;
+        color: #181818;
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,164 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <div class="promotion">
+          <h2 class="promotion__header">{{ promotion__header }}</h2>
+        </div>
+        <div class="website-title">
+          <h2 class="website-title__header">JordanShoes</h2>
+        </div>
+        <header>
+          <div class="header-texts">
+            <h2 class="header__title">A melhor loja de Jordan</h2>
+            <p class="header__paragraph">O tenis jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan.</p> 
+          </div>
+        </header>
+        <section class="products">
+          <h2 class="products__title">Destaques</h2>
+          <p class="products__paragraph">Frete gratis e chinelo de brinde é só aqui, aproveite por tempo limitado.</p>
+          <div class="products-items">
+            <shoes></shoes>
+          </div>
+        </section>
+        <footer>
+          <span class="footer__span">Todos os direitos reservados.</span>
+        </footer>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  import shoes from '../components/shoes';
+  export default {
+    name: 'Home',
+    created() {
+      document.title = 'JordanShoes';
+    },
+    components: {
+      shoes
+    },
+    data() {
+      return {
+        promotion__header: 'Ganhe R$10,00 de desconto no frete.'
+      }
+    }
   }
-}
 </script>
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  body {
+    width: auto;
+    font-family: 'Montserrat', sans-serif;
+  }
+  .container {
+    width: 100%;
+    height: auto;
+  }
+  .promotion {
+      height: 60px;
+      width: 100%;
+      background-color: #181818;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  }
+  .promotion__header {
+      font-size: 1rem;
+      font-weight: 500;
+      color: #f9f9f9;
+  }
+  .website-title {
+    width: 100%;
+    height: 50px;
+    background-color: #F6F6F6;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .website-title__header {
+    font-weight: 600;
+    font-size: 1.5rem;
+  }
+  header {
+    height: 378px;
+    width: 100%;
+    background-image: url('../assets/header-bg.jpeg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    color: #f6f6f6;
+  }
+  header::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 378px;
+    background: rgb(0, 0, 0, 0.6);
+    z-index: 1;
+  }
+  .header-texts {
+    width: 85%;
+    max-width: 1200px;
+    height: 100%;
+    margin: 0 auto;
+    position: relative;
+    color: #f9f9f9;
+    z-index: 2;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: left;
+  }
+  .header__title {
+    font-size: 2rem;
+  }
+  .header__paragraph {
+    font-size: 1.2rem;
+    font-weight: 500;
+    letter-spacing: .5px;
+    line-height: 30px;
+    width: 50%;
+  }
+  /* products section */
+  .products {
+    height: auto;
+    width: 85%;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 3em 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .products__title {
+    font-size: 2rem;
+    font-weight: 600;
+  }
+  .products__paragraph {
+    font-weight: 500;
+    font-size: 1.1rem;
+    margin: 1em 0;
+  }
+  .products-items {
+    width: 100%;
+  }
+  footer {
+    width: 100%;
+    height: 60px;
+    background-color: #181818;
+    color: #f6f6f6;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .footer_span {
+    font-weight: 500;
+    font-size: 1.25rem;
+  }
+</style>
